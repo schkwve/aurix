f8ea8e14ddf6a187640170caf8255d9ab9013f7f
Jozef Nagy <schkwve@gmail.com>
2024-01-20 12:30:27 +0100

    Update LICENSE

--- boot/generic-pc/x86_64/lib/efi.h
+++ boot/generic-pc/x86_64/lib/efi.h
@@ -1,29 +1,3 @@
-/************************************************************************
- * FILENAME:         efi.h
- *
- * DESCRIPTION:
- *       EFI Library header file. Structures and function pointers are
- *       defined here.
- *
- * AUTHOR:     Jozef Nagy                  DATE:    02 Jan 2024
- *
- * COPYRIGHT:
- *       Copyright (C) 2023, 2024  Jozef Nagy.
- *
- *       This program is free software; you can redistribute it and/or
- *       modify it under the terms of the GNU General Public License
- *       as published by the Free Software Foundation; either version 2
- *       of the License, or (at your option) any later version.
- *       
- *       This program is distributed in the hope that it will be useful,
- *       but WITHOUT ANY WARRANTY; without even the implied warranty of
- *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *       GNU General Public License for more details.
- *       
- *       You should have received a copy of the GNU General Public License
- *       along with this program; If not, see <http://www.gnu.org/licenses/>.
- **/
-
 #ifndef EFI_H
 #define EFI_H
 
